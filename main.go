14fe7b82eacb742b8fba1e4b3f62e1bc6a291d39
Rorie Clarke <rorie@Rories-MacBook-Pro.local>
2021-06-23 14:34:02 +0100

    added error messages

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -25,11 +26,32 @@ func addValue(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+
+	f, err := divideValue(100.0, 10.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by zero")
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 10.0, f))
+
+}
+
+func divideValue(x, y float32) (float32, error) {
+	if y <= x {
+		err := errors.New("Cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+
+}
+
 func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
 	http.HandleFunc("/euros", Euros)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting on port number: %s", portNumber))
 
