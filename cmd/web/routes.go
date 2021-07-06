9b4b79fc3742805b5dd4eccdf73ce9a753fde60b
rhc07 <rorie@ori.co>
2021-07-06 18:00:26 +0100

    added chi routers and middle ware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,17 +3,26 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/rhc07/basic-web-app/pkg/config"
 	"github.com/rhc07/basic-web-app/pkg/handlers"
 )
 
 func Routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
-	mux.Get("/euros", http.HandlerFunc(handlers.Repo.Euros))
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/euros", http.HandlerFunc(handlers.Repo.Euros))
+
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/euros", handlers.Repo.Euros)
 
 	return mux
 }
